71fc4b15cc71d0b376f10807660f490711451ec3
You-chun A. Huang <65477767+CannibalPride@users.noreply.github.com>
2023-11-24 02:18:18 +0800

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -6,6 +6,35 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint public constant MAX_BALANCE = 1000;  // Maximum allowed balance 
+
+    struct AccountInfo {
+        string name;           // Name of the account holder
+        address accountAddress; // address associated with the account
+    }
+
+    mapping(address => AccountInfo) public accounts; // Mapping to store account information for each address
+    
+    // Function to create an account with a given name
+    function createAccount(string memory name) public {
+        address accountAddress = msg.sender;
+        
+        // Check if an account already exists for the sender's address
+        require(bytes(accounts[accountAddress].name).length == 0, "Account already exists");
+        
+        // Create a new account using the sender's address and provided name
+        accounts[accountAddress] = AccountInfo(name, accountAddress);
+    }
+
+    function resetAccountName() public {
+        address accountAddress = msg.sender;
+
+        // Check if an account exists for the sender's address
+        require(bytes(accounts[accountAddress].name).length > 0, "Account does not exist");
+
+        // Reset the account name to an empty string
+        accounts[accountAddress].name = "";
+    }
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
@@ -19,11 +48,22 @@ contract Assessment {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+    function getMaxBalance() public pure returns(uint256){
+        return MAX_BALANCE;
+    }
 
+    function getCurrentAccountName() public view returns (string memory) {
+        return accounts[owner].name;
+    }
+
+    modifier isOwner() {
         // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
+
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
 
         // perform transaction
         balance += _amount;
@@ -38,8 +78,7 @@ contract Assessment {
     // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
+    function withdraw(uint256 _withdrawAmount) public isOwner{
         uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
